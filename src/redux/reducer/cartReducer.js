3cdb92715d1ac5a89e46d6de6949a1a04f2cff2e
SimonJay <szyszkakontrast@gmail.com>
2022-01-04 15:41:14 +0100

    removig error

--- src/redux/reducer/cartReducer.js
+++ src/redux/reducer/cartReducer.js
@@ -1,5 +1,3 @@
-import * as actionTypes from '../constants/cartConstants'
-
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case 'ADD_TO_CART':
